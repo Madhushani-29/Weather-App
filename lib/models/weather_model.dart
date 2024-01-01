760b28b79bd9a2ac5b04634bb35e1ad5f604e477
Madhushani-29 <madhushani2904@gmail.com>
2024-01-01 21:20:16 +0530

    weather app data model

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,8 @@
+class WeatherModel {
+  final String currentSky;
+  final double currentPressure;
+  final double currentWindSpeed;
+  final double currentHumidity;
+
+  WeatherModel(this.currentSky, this.currentPressure, this.currentWindSpeed, this.currentHumidity);
+}
