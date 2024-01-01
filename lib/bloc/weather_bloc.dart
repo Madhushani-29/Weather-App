fd8dd003ee6b32333c6c63ce15ca4b75a1ed6aa4
Madhushani-29 <madhushani2904@gmail.com>
2024-01-01 22:07:32 +0530

    weather app bloc

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,13 +1,16 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:weather_app/data/repository/weather_repository.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc() : super(WeatherInitial()) {
-    on<WeatherEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final WeatherRepository weatherRepository;
+
+  WeatherBloc(this.weatherRepository) : super(WeatherInitial()) {
+    on<WeatherFetched>(_getCurrentWeather);
   }
+
+  void _getCurrentWeather(WeatherFetched event, Emitter<WeatherState> emit) {}
 }
