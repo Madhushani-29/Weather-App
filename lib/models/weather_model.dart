5b30c29807b88d5468bdfa52e2ca090058226f0b
Madhushani-29 <madhushani2904@gmail.com>
2024-01-01 21:23:22 +0530

    weather app data model

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class WeatherModel {
   final String currentSky;
@@ -5,11 +7,78 @@ class WeatherModel {
   final double currentWindSpeed;
   final double currentHumidity;
   final double currentTemp;
-  WeatherModel(
-    this.currentSky,
-    this.currentPressure,
-    this.currentWindSpeed,
-    this.currentHumidity,
-    this.currentTemp,
-  );
+  
+  WeatherModel({
+    required this.currentSky,
+    required this.currentPressure,
+    required this.currentWindSpeed,
+    required this.currentHumidity,
+    required this.currentTemp,
+  });
+
+  WeatherModel copyWith({
+    String? currentSky,
+    double? currentPressure,
+    double? currentWindSpeed,
+    double? currentHumidity,
+    double? currentTemp,
+  }) {
+    return WeatherModel(
+      currentSky: currentSky ?? this.currentSky,
+      currentPressure: currentPressure ?? this.currentPressure,
+      currentWindSpeed: currentWindSpeed ?? this.currentWindSpeed,
+      currentHumidity: currentHumidity ?? this.currentHumidity,
+      currentTemp: currentTemp ?? this.currentTemp,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'currentSky': currentSky,
+      'currentPressure': currentPressure,
+      'currentWindSpeed': currentWindSpeed,
+      'currentHumidity': currentHumidity,
+      'currentTemp': currentTemp,
+    };
+  }
+
+  factory WeatherModel.fromMap(Map<String, dynamic> map) {
+    return WeatherModel(
+      currentSky: map['currentSky'] as String,
+      currentPressure: map['currentPressure'] as double,
+      currentWindSpeed: map['currentWindSpeed'] as double,
+      currentHumidity: map['currentHumidity'] as double,
+      currentTemp: map['currentTemp'] as double,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory WeatherModel.fromJson(String source) =>
+      WeatherModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'WeatherModel(currentSky: $currentSky, currentPressure: $currentPressure, currentWindSpeed: $currentWindSpeed, currentHumidity: $currentHumidity, currentTemp: $currentTemp)';
+  }
+
+  @override
+  bool operator ==(covariant WeatherModel other) {
+    if (identical(this, other)) return true;
+
+    return other.currentSky == currentSky &&
+        other.currentPressure == currentPressure &&
+        other.currentWindSpeed == currentWindSpeed &&
+        other.currentHumidity == currentHumidity &&
+        other.currentTemp == currentTemp;
+  }
+
+  @override
+  int get hashCode {
+    return currentSky.hashCode ^
+        currentPressure.hashCode ^
+        currentWindSpeed.hashCode ^
+        currentHumidity.hashCode ^
+        currentTemp.hashCode;
+  }
 }
